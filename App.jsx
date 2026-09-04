48d940b1d574876f4cd69b4a02cf5f580e42c888
Roshan-r2024 <roshan.r2024@vitstudent.ac.in>
2026-09-04 22:57:10 +0530

    Fix duplicate item details route

--- App.jsx
+++ App.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import GetStarted from "./pages/GetStarted";
 import Login from "./pages/Login";
@@ -21,78 +16,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* FIRST PAGE */}
-        <Route
-          path="/"
-          element={<Navigate to="/get-started" replace />}
-        />
-
-        {/* GET STARTED */}
-        <Route
-          path="/get-started"
-          element={<GetStarted />}
-        />
-
-        {/* AUTHENTICATION */}
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-
-        <Route
-          path="/signup"
-          element={<SignUp />}
-        />
-
-        {/* HOME */}
-        <Route
-          path="/home"
-          element={<Home />}
-        />
-
-        {/* BROWSE ITEMS */}
-        <Route
-          path="/browse"
-          element={<Browse />}
-        />
-
-        {/* LIST AN ITEM */}
-        <Route
-          path="/list-item"
-          element={<ListItem />}
-        />
-
-        {/* ITEM DETAILS */}
-       <Route
-  path="/item-details/:id"
-  element={<ItemDetails />}
-/>
-<Route path="/item-details" element={<ItemDetails />} />
-        {/* MY ITEMS */}
-        <Route
-          path="/my-items"
-          element={<MyItems />}
-        />
-
-        {/* PROFILE */}
-        <Route
-          path="/profile"
-          element={<Profile />}
-        />
-
-        {/* BORROW REQUEST */}
-        <Route
-          path="/borrow-request"
-          element={<BorrowRequest />}
-        />
-
-        {/* UNKNOWN URL */}
-        <Route
-          path="*"
-          element={<Navigate to="/get-started" replace />}
-        />
-
+        <Route path="/" element={<Navigate to="/get-started" replace />} />
+        <Route path="/get-started" element={<GetStarted />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route path="/list-item" element={<ListItem />} />
+        <Route path="/item-details/:id" element={<ItemDetails />} />
+        <Route path="/my-items" element={<MyItems />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/borrow-request" element={<BorrowRequest />} />
+        <Route path="*" element={<Navigate to="/get-started" replace />} />
       </Routes>
     </BrowserRouter>
   );
