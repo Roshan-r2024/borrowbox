99bd5f3d5c6aa60210b172354bc8948fa4079c78
Roshan-r2024 <roshan.r2024@vitstudent.ac.in>
2026-09-14 18:50:40 +0530

    apply Flowbite-inspired Borrow Box layout

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 import "./App.css";
 import "./pages/HomeBrightTheme.css";
 import "./AppTheme.css";
+import "./BorrowBoxLayout.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
